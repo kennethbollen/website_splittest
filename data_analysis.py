d6eb6bd5aeb168d138c917e895096719c540e6e7
kennethbollen <31512761+kennethbollen@users.noreply.github.com>
2018-01-30 18:05:37 +0000

    Update data_analysis.py

--- data_analysis.py
+++ data_analysis.py
@@ -3,12 +3,33 @@ import numpy as np
 from data_extract import *
 from stats_functions import *
 
+#converstions
+webA_conv = []
+webB_conv = []
+for index, row in split_test.iterrows():
+	webA_conv.append(row['Website_A']['Orders'] / row['Website_A']['Visits'])
+	webB_conv.append(row['Website_B']['Orders'] / row['Website_B']['Visits'])
+  
 #EDA
+bar_xlabel = []
+for i in split_test.index:
+	bar_xlabel.append(i)
+
 N = len(split_test)
 ind = np.arange(N)
 width = 0.35
 fig, ax = plt.subplots()
-
+rects1 = ax.bar(ind, webA_conv, width, color='r')
+rects2 = ax.bar(ind + width, webB_conv, width, color='y')
+ax.set_ylabel('Conversion Rate %')
+ax.set_title('Two-week split test on conversion rate')
+ax.set_xticks(ind + width / 2)
+ax.set_xticklabels(bar_xlabel)
+ax.legend((rects[0], rects[0]), ('Website A', 'Website B'))
+ax.legend((rects1[0], rects2[0]), ('Website A', 'Website B'))
+autolabel(rects1)
+autolabel(rects2)
+plt.show()
 
 #choose significance level 
 sig_level = 0.05
