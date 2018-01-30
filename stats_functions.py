fc629dec60a7d39faa204109e2987b30b53e9d8a
kennethbollen <31512761+kennethbollen@users.noreply.github.com>
2018-01-30 11:16:42 +0000

    Update stats_functions.py

--- stats_functions.py
+++ stats_functions.py
@@ -1,10 +1,18 @@
 import numpy as np
 
+#calculate the conversion for the two-week split trial
+def diff_frac_obs(data_A, data_B):
+	frac_A = np.sum(data_A['Orders']) / np.sum(data_A['Visits'])
+	frac_B = np.sum(data_B['Orders']) / np.sum(data_B['Visits'])
+	return frac_B - frac_A
+
+#calculate the conversion for the permuatation tests
 def diff_frac(data_A, data_B):
     frac_A = np.sum(data_A) / np.sum(split_test['Website_A']['Visits'])
     frac_B = np.sum(data_B) / np.sum(split_test['Website_B']['Visits'])
     return frac_B - frac_A
 
+#Create a permuation sample through randomisation
 def permutation_sample(data1, data2):
     """Generate a permutation sample from two data sets."""
 
@@ -19,7 +27,8 @@ def permutation_sample(data1, data2):
     perm_sample_2 = permuted_data[len(data1['Visits']):]
 
     return perm_sample_1, perm_sample_2
-    
+
+#Calculate the conversion rate of the permuation sample 10000 times 
 def draw_perm_reps(data_1, data_2, func, size=1):
     """Generate multiple permutation replicates."""
 
