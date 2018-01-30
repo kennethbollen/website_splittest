8e677b5f94746eb5a6015a677dd2d315c61b6d81
kennethbollen <31512761+kennethbollen@users.noreply.github.com>
2018-01-30 13:12:12 +0000

    Update data_analysis.py
    added EDA

--- data_analysis.py
+++ data_analysis.py
@@ -1,10 +1,19 @@
+import matplotlib.pyplot as plt
+import numpy as np
 from data_extract import *
 from stats_functions import *
+
+#EDA
+N = len(split_test)
+ind = np.arange(N)
+width = 0.35
+fig, ax = plt.subplots()
+
 
 #choose significance level 
 sig_level = 0.05
 
-#calcualte the observed converstion rate of the two-week trial
+#calculate the observed converstion rate of the two-week trial
 obs_diff = diff_frac_obs(split_test['Website_A'], split_test['Website_B'])
 
 #permutate the data for 10000 reps
@@ -14,4 +23,7 @@ draw_perm_reps(split_test['Website_A'], split_test['Website_B'], diff_frac, 1000
 p_value = np.sum(perm_replicates >= obs_diff) / 10000
 
 if p_value > sig_level:
-
+  print('Accept null hypothesis: The two-week promotion had no effect on conversion')
+else:
+  print('Reject null hypothesis: The two-week promotion had a effect on conversion')
+  
