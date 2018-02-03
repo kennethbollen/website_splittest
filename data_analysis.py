122b3310fda42f641bbca1e2900d3552cf0580d6
kennethbollen <31512761+kennethbollen@users.noreply.github.com>
2018-02-03 16:48:59 +0000

    Update data_analysis.py

--- data_analysis.py
+++ data_analysis.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from data_extract import *
-#from stats_functions import *
 
+webA_conv = []
+webB_conv = []
 cpc = 1.50
 bronze_cpc = 0.50
 silver_cpc = 1.50
@@ -52,74 +53,41 @@ def draw_perm_reps(data_1, data_2, func, size=1):
 
     return perm_replicates
 
-'''#converstions
-webA_conv = []
-webB_conv = []
-for index, row in split_test.iterrows():
-	webA_conv.append(row['Website_A']['Orders'] / row['Website_A']['Visits'])
-	webB_conv.append(row['Website_B']['Orders'] / row['Website_B']['Visits'])
-  
-#EDA on orders
-bar_xlabel = []
-for i in split_test.index:
-	bar_xlabel.append(i)
-	
-N = len(split_test)
-ind = np.arange(N)
-width = 0.35
-fig, ax = plt.subplots()
-rects1 = ax.bar(ind, split_test['Website_A']['Orders'], width, color='r')
-rects2 = ax.bar(ind + width, split_test['Website_B']['Orders'], width, color='y')
-ax.set_ylabel('Number of Orders')
-ax.set_xlabel('Two Week Period')
-ax.set_title('Two-week split test impact on orders')
-ax.set_xticks(ind + width / 2)
-ax.set_xticklabels(bar_xlabel)
-
-ax2 = ax.twinx()
-line1 = ax2.plot(ax.get_xticks(), webA_conv , linestyle='-', marker='o', color='r', linewidth=1.0)
-line2 = ax2.plot(ax.get_xticks(), webB_conv , linestyle='-', marker='o', color='y', linewidth=1.0)
-
-vals = ax2.get_yticks()
-ax2.set_yticklabels(['{:3.2f}%'.format(x*100) for x in vals])
-ax2.set_ylabel('Conversion Rate %')
-
-ax.legend((rects1[0], rects2[0], line1[0], line2[0]), ('Web A Orders', 'Web B Orders', 'Web A Conversion', 'Web B Conversion'))
-
-def autolabel(rects):
-    """
-    Attach a text label above each bar displaying its height
-    """
-    for rect in rects:
-        height = rect.get_height()
-        ax.text(rect.get_x() + rect.get_width()/2., 1.05*height,
-                '%d' % int(height),
-                ha='center', va='bottom')
-
-autolabel(rects1)
-autolabel(rects2)
-plt.show()'''
-
 #choose significance level 
 sig_level = 0.05
+print('Chosen significance level: ', sig_level)
+print()
 
 #initialize the array to capture the permutations of the replications of the test
 perm_replicates = np.empty(10000) 
+print('Number of permutations for the test: ', len(perm_replicates))
+print()
 
 #calculate the observed converstion rate of the two-week trial
 obs_diff = diff_frac_obs(split_test['Website_A'], split_test['Website_B'])
+print('The observed converstion rate difference between website A and website B: ', obs_diff)
+print()
 
 #permutate the data for 10000 reps
+print('Setting up test...')
+print()
 draw_perm_reps(split_test['Website_A'], split_test['Website_B'], diff_frac, 10000)
 
 #calculate the p-value
+print('Conducting test...')
+print()
 p_value = np.sum(perm_replicates >= obs_diff) / 10000
 
 if p_value > sig_level:
   print('p value: ', p_value,'\nAccept null hypothesis: The two-week promotion had no effect on conversion')	
 else:
   print('p value', p_value,'\nReject null hypothesis: The two-week promotion had a effect on conversion')
-  
+
+#converstions
+for index, row in split_test.iterrows():
+	webA_conv.append(row['Website_A']['Orders'] / row['Website_A']['Visits'])
+	webB_conv.append(row['Website_B']['Orders'] / row['Website_B']['Visits'])
+
 #current plan
 ctr = split_test['Website_A']['Visits'].sum() + split_test['Website_B']['Visits'].sum()
 avg_conv = np.mean(webA_conv)
